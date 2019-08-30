01fe1e10c147bfabab3f33ac9c068cfe44f767bf
DawidKopicki <dawidkopicki@gmail.com>
2019-08-30 14:48:17 +0200

    Creating first element in a different way

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = React.createElement(
-    'div',
-    {
-        className: 'wrapper'
-    },
-    [
-        React.createElement(
-            'h1',
-            {
-                className: 'mainHeader'
-            },
-            'Hello world'
-        ),
-        React.createElement(
-            'h2',
-            {
-                className: 'secondaryHeader'
-            },
-            'Hello world?'
-        ),
-    ]
+const App = () => (
+    <div>
+        <h1 className="mainHeader">Hello world</h1>
+        <h2 className="secondaryHeader">Hello world?</h2>
+    </div>
 );
 
 ReactDOM.render(
     //rendered element
-    App,
+    <App />,
     //target
     document.getElementById('root')
 );
