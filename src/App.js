5e14df09461788ddd71cb03278c727e54a182b63
DawidKopicki <dawidkopicki@gmail.com>
2019-09-03 14:44:31 +0200

    Creating sample class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css'
+import './components/MyComponent/MyComponent'
+import MyComponent from './components/MyComponent/MyComponent';
 
 
 const App = () => (
     <div>
+        <MyComponent />
         <ListWrapper />
     </div>
 );
