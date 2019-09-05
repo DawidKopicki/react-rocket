a7e62422a7f010d7d12aec5e39b1fcf2f2a5bbec
DawidKopicki <dawidkopicki@gmail.com>
2019-09-05 15:45:49 +0200

    Navigation styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Navigation.module.scss'
 
 const Navigation = () => (
-    <ul>
-        <li><Link to="/">Twitters</Link></li>
-        <li><Link to="/articles">Articles</Link></li>
-        <li><Link to="/notes">Notes</Link></li>
-    </ul>
+    <nav>
+        <ul className={styles.wrapper}>
+            <li className={styles.navigationItem}>
+                <Link className={styles.navigationItemLink} to="/">Twitters</Link>
+            </li>
+            <li className={styles.navigationItem}>
+                <Link className={styles.navigationItemLink} to="/articles">Articles</Link>
+            </li>
+            <li className={styles.navigationItem}>
+                <Link className={styles.navigationItemLink} to="/notes">Notes</Link>
+            </li>
+        </ul>
+    </nav>
 );
 
 export default Navigation;
