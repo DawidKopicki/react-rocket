26218478bef11cda944ba7c6481cd71e05b9974d
DawidKopicki <dawidkopicki@gmail.com>
2019-09-06 13:56:52 +0200

    Styling using NavLink

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.scss'
 
 const Navigation = () => (
     <nav>
         <ul className={styles.wrapper}>
-            <li className={styles.navigationItem}>
-                <Link className={styles.navigationItemLink} to="/">Twitters</Link>
+            <li className={styles.navItem}>
+                <NavLink exact activeClassName={styles.navItemLinkActive} className={styles.navItemLink} to="/">twitters</NavLink>
             </li>
-            <li className={styles.navigationItem}>
-                <Link className={styles.navigationItemLink} to="/articles">Articles</Link>
+            <li className={styles.navItem}>
+                <NavLink activeClassName={styles.navItemLinkActive} className={styles.navItemLink} to="/articles">articles</NavLink>
             </li>
-            <li className={styles.navigationItem}>
-                <Link className={styles.navigationItemLink} to="/notes">Notes</Link>
+            <li className={styles.navItem}>
+                <NavLink activeClassName={styles.navItemLinkActive} className={styles.navItemLink} to="/notes">notes</NavLink>
             </li>
         </ul>
     </nav>
